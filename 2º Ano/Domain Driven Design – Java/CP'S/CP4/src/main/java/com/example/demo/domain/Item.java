517ac32509ae0c8dec299942fc326f177b44b874
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2026-03-25 18:58:29 -0300

    Add Item class with properties and constructors

--- 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/domain/Item.java
+++ 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/domain/Item.java
@@ -1,1 +1,23 @@
+package com.example.demo.domain;
 
+public class Item {
+
+    private String nome;
+    private int quantidade;
+
+    public Item() {
+    }
+
+    public Item(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
