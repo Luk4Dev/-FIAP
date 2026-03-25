74436e0796286f17c54e631e997aad4442b73611
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2026-03-25 18:58:50 -0300

    Update PedidoService.java

--- 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/service/PedidoService.java
+++ 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/service/PedidoService.java
@@ -1,1 +1,48 @@
+package com.example.demo.service;
 
+import com.example.demo.domain.Item;
+import com.example.demo.domain.Pedido;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    public Pedido iniciarPedido() {
+        return new Pedido();
+    }
+
+    public Pedido adicionarItem(Pedido pedido, Item item) {
+        pedido.adicionarItem(item);
+        return pedido;
+    }
+
+    public Pedido removerItem(Pedido pedido, String nome) {
+        pedido.removerItem(nome);
+        return pedido;
+    }
+
+    public Pedido confirmarPedido(Pedido pedido) {
+        pedido.confirmar();
+        return pedido;
+    }
+
+    public Pedido iniciarPreparo(Pedido pedido) {
+        pedido.iniciarPreparo();
+        return pedido;
+    }
+
+    public Pedido finalizarPreparo(Pedido pedido) {
+        pedido.finalizarPreparo();
+        return pedido;
+    }
+
+    public Pedido entregar(Pedido pedido) {
+        pedido.entregar();
+        return pedido;
+    }
+
+    public Pedido cancelar(Pedido pedido) {
+        pedido.cancelar();
+        return pedido;
+    }
+}
