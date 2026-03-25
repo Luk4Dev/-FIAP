b72192e8d0d3eb4d9624df2d59fed0cc8c73ff61
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2026-03-25 18:59:08 -0300

    Update PedidoController.java

--- 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/controller/PedidoController.java
+++ 2º Ano/Domain Driven Design – Java/CP'S/CP4/src/main/java/com/example/demo/controller/PedidoController.java
@@ -1,1 +1,58 @@
+package com.example.demo.controller;
 
+import com.example.demo.domain.Item;
+import com.example.demo.domain.Pedido;
+import com.example.demo.service.PedidoService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedido")
+public class PedidoController {
+
+    private final PedidoService service;
+
+    public PedidoController(PedidoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/iniciar")
+    public Pedido iniciar() {
+        return service.iniciarPedido();
+    }
+
+    @PostMapping("/adicionar-item")
+    public Pedido adicionarItem(@RequestBody Pedido pedido, @RequestParam String nome, @RequestParam int quantidade) {
+        Item item = new Item(nome, quantidade);
+        return service.adicionarItem(pedido, item);
+    }
+
+    @PostMapping("/remover-item")
+    public Pedido removerItem(@RequestBody Pedido pedido, @RequestParam String nome) {
+        return service.removerItem(pedido, nome);
+    }
+
+    @PostMapping("/confirmar")
+    public Pedido confirmar(@RequestBody Pedido pedido) {
+        return service.confirmarPedido(pedido);
+    }
+
+    @PostMapping("/iniciar-preparo")
+    public Pedido iniciarPreparo(@RequestBody Pedido pedido) {
+        return service.iniciarPreparo(pedido);
+    }
+
+    @PostMapping("/finalizar-preparo")
+    public Pedido finalizarPreparo(@RequestBody Pedido pedido) {
+        return service.finalizarPreparo(pedido);
+    }
+
+    @PostMapping("/entregar")
+    public Pedido entregar(@RequestBody Pedido pedido) {
+        return service.entregar(pedido);
+    }
+
+    @PostMapping("/cancelar")
+    public Pedido cancelar(@RequestBody Pedido pedido) {
+        return service.cancelar(pedido);
+    }
+}
